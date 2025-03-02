0c7d22c684ae3b6b66a9a05b747018ce92fd8a71
Gowthami-ui <gowthamicse1998@gmail.com>
2025-03-02 18:18:17 +0530

    Add files via upload

---
+++ admin/src/context/AppContext.jsx
@@ -0,0 +1,45 @@
+import { createContext } from "react";
+
+
+export const AppContext = createContext()
+
+const AppContextProvider = (props) => {
+
+    const currency = import.meta.env.VITE_CURRENCY
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    const slotDateFormat = (slotDate) => {
+        const dateArray = slotDate.split('_')
+        return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+    }
+
+    const calculateAge = (dob) => {
+        if (!dob) return '';
+    
+        const birthDate = new Date(dob);
+        if (isNaN(birthDate.getTime())) return ''; 
+    
+        const today = new Date();
+        let age = today.getFullYear() - birthDate.getFullYear();
+    
+        return age;
+    };
+
+    const value = {
+        backendUrl,
+        currency,
+        slotDateFormat,
+        calculateAge,
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+
+}
+
+export default AppContextProvider
